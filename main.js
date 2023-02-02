974d64531584f2d5caecd851f628594ec7514f48
Vihaan1237 <97681933+Vihaan1237@users.noreply.github.com>
2023-02-02 17:44:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function login() {
+    window.location="choosecinema.html";
+}
+
+function back(){
+    window.location="choosecinema.html";
+}
+
+function selecttime(){
+    window.location="bookingtime.html";
+}
+
+function time(){
+    window.location="buy.html";
+}
+function chooseseat1(){
+    window.location="wfe.html";
+}
+
+function chooseseat2(){
+    window.location="a2.html";
+}
+
+function chooseseat3(){
+    window.location="ba.html";
+}
+
+function chooseseat4(){
+    window.location="DSitmom.html";
+}
+
+function chooseseat5(){
+    window.location="m.html";
+}
+
+function chooseseat6(){
+    window.location="ns.html";
+}
+
+function chooseseat7(){
+    window.location="op.html";
+}
+
+function complete(){
+    window.location="complete.html";
+}
+
+function complete(){
+    window.location="complete.html";
+}
+
+function back2(){
+    window.location="index.html";
+}
